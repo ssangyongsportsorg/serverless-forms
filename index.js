ef24153d8b79d9f50eb6fa3abfddddb0637051ac
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-02-17 19:40:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var nodemailer = require('nodemailer');
 // setup the server
 // listen on port specified by the `PORT` env var
 var server = http.createServer(function (req, res) {
-    if (req.method.toLowerCase() == 'get') {
+    if (req.method.toLowerCase() === 'get') {
         displayForm(res);
-    } else if (req.method.toLowerCase() == 'post') {
+    } else if (req.method.toLowerCase() === 'post') {
         processFormFieldsIndividual(req, res);
     }
 });
@@ -34,29 +34,25 @@ function displayForm(res) {
 // get the POST data 
 // and call the sendMail method
 function processFormFieldsIndividual(req, res) {
-    var fields = [];
     var form = new formidable.IncomingForm();
-    form.on('field', function (field, value) {
-        console.log(field);
-        console.log(value);
-        fields[field] = value;
-    });
-
-    form.on('end', function () {
+    form.parse(req, function (err, fields) {
+        if (err) {
+            console.error(err);
+        } else {
+            var replyTo = fields['Email'];
+            sendMail(util.inspect(fields), replyTo);
+        }
         res.writeHead(302, {
             'Location': 'https://ssangyongsports.eu.org/thanks'
         });
         res.end();
-        var replyTo = fields['email'];
-        sendMail(util.inspect(fields), replyTo);
     });
-    form.parse(req);
 }
 
 let transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_PORT === 465,
+    secure: process.env.EMAIL_PORT === '465',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
@@ -71,7 +67,7 @@ function sendMail(text, replyTo) {
         subject: 'New form submission' + (process.env.SITE_NAME ? ' on ' + process.env.SITE_NAME : ''),
         text: text
     };
-    console.log('sending email: ', mailOptions);
+    console.log('sending email:', mailOptions);
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {

500bc57dad1efcfcfa6ca5a878e64fb16c5952d4
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-03-29 21:31:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var nodemailer = require('nodemailer');
 // setup the server
 // listen on port specified by the `PORT` env var
 var server = http.createServer(function (req, res) {
-    if (req.method.toLowerCase() === 'get') {
-        displayForm(res);
-    } else if (req.method.toLowerCase() === 'post') {
-        processFormFieldsIndividual(req, res);
-    }
+  if (req.method.toLowerCase() === 'get') {
+    displayForm(res);
+  } else if (req.method.toLowerCase() === 'post') {
+    processFormFieldsIndividual(req, res);
+  }
 });
 var port = process.env.PORT || 8080;
 server.listen(port);
@@ -21,59 +21,68 @@ console.log("server listening on ", port);
 // serve HTML file
 // located according to the `FORM` env var
 function displayForm(res) {
-    fs.readFile(process.env.FORM || 'form.html', function (err, data) {
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
-            'Content-Length': data.length
-        });
-        res.write(data);
-        res.end();
+  fs.readFile(process.env.FORM || 'form.html', function (err, data) {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Content-Length': data.length
     });
+    res.write(data);
+    res.end();
+  });
 }
 
 // get the POST data 
 // and call the sendMail method
 function processFormFieldsIndividual(req, res) {
+  var referer = req.headers.referer || '';
+  if (referer.startsWith('https://ssangyongsports.su.org')) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields) {
-        if (err) {
-            console.error(err);
-        } else {
-            var replyTo = fields['Email'];
-            var subject = fields['Subject'];
-            sendMail(util.inspect(fields), replyTo, subject);
-        }
-        res.writeHead(302, {
-            'Location': 'https://ssangyongsports.eu.org/thanks'
-        });
-        res.end();
+      if (err) {
+        console.error(err);
+      } else {
+        var replyTo = fields['Email'];
+        var subject = fields['Subject'];
+        sendMail(util.inspect(fields), replyTo, subject);
+      }
+      res.writeHead(302, {
+        'Location': 'https://ssangyongsports.eu.org/thanks'
+      });
+      res.end();
     });
+  } else {
+    res.writeHead(403, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('Forbidden');
+    res.end();
+  }
 }
 
 let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_PORT === '465',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: process.env.EMAIL_PORT === '465',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 });
 
 function sendMail(text, replyTo, subject) {
-    let mailOptions = {
-        from: process.env.FROM || 'Email form data bot <no-reply@no-email.com>',
-        to: [process.env.TO, process.env.TO2],
-        replyTo: replyTo,
-        subject: subject,
-        text: text
-    };
-    console.log('sending email:', mailOptions);
+  let mailOptions = {
+    from: process.env.FROM || 'Email form data bot <no-reply@no-email.com>',
+    to: [process.env.TO, process.env.TO2],
+    replyTo: replyTo,
+    subject: subject,
+    text: text
+  };
+  console.log('sending email:', mailOptions);
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message %s sent: %s', info.messageId, info.response);
-    });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
 }

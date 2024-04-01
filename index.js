00dc74c81461c95a21894ded1f3276ab465214bc
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-01 23:38:14 +0800

    Update index.jsm

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const formidable = require("formidable");
 const util = require('util');
 const nodemailer = require('nodemailer');
+const querystring = require('querystring');
 
 // setup the server
 const server = http.createServer(function (req, res) {
@@ -32,19 +33,36 @@ function displayForm(res) {
 }
 
 // get the POST data and call the sendMail method
-function processFormFieldsIndividual(req, res) {
+async function processFormFieldsIndividual(req, res) {
   const referer = req.headers.referer || '';
   const clientIP = req.socket.remoteAddress; // 获取客户端 IP 地址
+  const userAgent = req.headers['user-agent'];
 
   if (referer.startsWith('https://ssangyongsports.eu.org')) {
     const form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields) {
+    form.parse(req, async function (err, fields) {
       if (err) {
         console.error(err);
       } else {
         // Check if honeypot field is filled
         if (fields['honeypot']) {
           console.log('Spam detected!');
+          res.writeHead(403, { 'Content-Type': 'text/plain' });
+          res.end('Ha ha, we caught you! Please stop sending this spam contact.');
+          return;
+        }
+
+        // Check with Akismet
+        const isSpam = await checkAkismet({
+          clientIP,
+          userAgent,
+          referrer,
+          message: fields['Message'],
+          otherFields: fields
+        });
+
+        if (isSpam) {
+          console.log('Spam detected by Akismet!');
           res.writeHead(403, { 'Content-Type': 'text/plain' });
           res.end('Ha ha, we caught you! Please stop sending this spam contact.');
           return;
@@ -95,5 +113,51 @@ function sendMail(text, replyTo, subject, clientIP) {
       return console.log(error);
     }
     console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+}
+
+async function checkAkismet(data) {
+  const akismetData = {
+    blog: 'https://ssangyongsports.eu.org',
+    user_ip: data.clientIP,
+    user_agent: data.userAgent,
+    referrer: data.referrer,
+    permalink: 'https://ssangyongsports.eu.org/contact',
+    comment_type: 'contact-form',
+    comment_content: data.message,
+    ...data.otherFields
+  };
+
+  const dataString = querystring.stringify(akismetData);
+
+  const options = {
+    hostname: 'rest.akismet.com',
+    port: 80,
+    path: '/1.1/comment-check',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': dataString.length
+    },
+    auth: 'process.env.Key'
+  };
+
+  return new Promise((resolve, reject) => {
+    const req = http.request(options, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        resolve(data === 'true');
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.write(dataString);
+    req.end();
   });
 }
